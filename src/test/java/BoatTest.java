221fc1838b57aebd18f1dbabe1d4bf5a2e09dfbb
Elizabeth <evelasco@thoughtworks.com>
2019-08-01 22:58:00 +0200

    write test conditions in boat file

--- src/test/java/BoatTest.java
+++ src/test/java/BoatTest.java
@@ -4,5 +4,9 @@ import static org.junit.Assert.*;
 
 public class BoatTest {
 
-
+//should create boat with size, and initial coordinate.
+// boat size should be bigger than 1
+// boat size should be smaller than 6
+// should generate other coordinates based on initial coordinate and size.
+// should validate coordinates to be within board limits (a9, a10, a11 is invalid)
 }
